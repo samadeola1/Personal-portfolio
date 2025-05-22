1562d4686491a6784207e1c82fc6d0bf60ab5515
samadeola1 <oroguns28@gmail.com>
2025-05-22 10:15:49 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./Layouts/Navbar";
+import Navbar from "./layouts/Navbar";
 import Hero from "./features/Hero";
 import Projects from "./features/Projects";
 import About from "./features/About";
